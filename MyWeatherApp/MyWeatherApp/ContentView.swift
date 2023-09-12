994336e31d0f59b8fbc0d6976c27c40f86d9df8a
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-09-12 15:37:44 -0400

    add image and text representing weather icon and temp to conent view

--- MyWeatherApp/MyWeatherApp/ContentView.swift
+++ MyWeatherApp/MyWeatherApp/ContentView.swift
@@ -12,6 +12,22 @@ struct ContentView: View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("West Palm Beach, FL")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .padding()
+                VStack(spacing: 8) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("89°")
+                        .font(.system(size: 70, weight: .medium))
+                }
+                Spacer()
+            }
         }
     }
 }
