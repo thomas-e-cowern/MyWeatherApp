86004f92107b11cb0ac116dd9a3dcbe2267454d3
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-09-12 13:46:21 -0400

    add xcode project to folder

---
+++ MyWeatherApp/MyWeatherApp/MyWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherAppApp.swift
+//  MyWeatherApp
+//
+//  Created by Thomas Cowern on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
